e689f8d3ea86adb70270342ea9351a914dffe5f4
Aleksandr Lvov <alexlvov26@gmail.com>
2023-03-11 11:52:11 +0200

    Moved Camera in separate module

--- src/lib.rs
+++ src/lib.rs
@@ -10,38 +10,6 @@ pub type Point3 = cgmath::Point3<f64>;
 pub type Vector3 = cgmath::Vector3<f64>;
 pub type Colour = cgmath::Vector3<f64>;
 
-pub struct Camera {
-    pub viewport_width: f64,
-    pub viewport_height: f64,
-    pub focal_length: f64,
-    pub origin: Point3,
-    pub horizontal: Vector3,
-    pub vertical: Vector3,
-    pub lower_left_corner: Point3,
-}
-
-impl Default for Camera {
-    fn default() -> Camera {
-        let viewport_height = 2.0;
-        let viewport_width = (1280.0 / 720.0) * 2.0;
-        let focal_length = 1.0;
-        let origin = Point3::new(0.0, 0.0, 0.0);
-        let horizontal = Vector3::new(viewport_width, 0.0, 0.0);
-        let vertical = Vector3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner =
-            origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0.0, 0.0, focal_length);
-        Camera {
-            viewport_width,
-            viewport_height,
-            focal_length,
-            origin,
-            horizontal,
-            vertical,
-            lower_left_corner,
-        }
-    }
-}
-
 pub struct Ray {
     origin: Point3,
     direction: Vector3,
