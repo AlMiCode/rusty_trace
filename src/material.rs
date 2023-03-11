7d8fd9e8a678a1a95ab733405a01d024c887f535
Aleksandr Lvov <alexlvov26@gmail.com>
2023-03-11 22:44:41 +0200

    fixed lambertian

--- src/material.rs
+++ src/material.rs
@@ -1,9 +1,11 @@
-use crate::{Ray, Colour, random_vec_in_hemisphere};
+use cgmath::InnerSpace;
+
 use crate::hittable::HitRecord;
+use crate::{random_vec_in_sphere, Colour, Ray};
 
 pub struct ScatterRecord {
     pub ray: Ray,
-    pub attenuation: Colour
+    pub attenuation: Colour,
 }
 
 pub trait Material {
@@ -11,15 +13,20 @@ pub trait Material {
 }
 
 pub struct Lambertian {
-    pub albedo: Colour
+    pub albedo: Colour,
 }
 
 impl Material for Lambertian {
     fn scatter(&self, _ray: &Ray, hit: &HitRecord) -> Option<ScatterRecord> {
-        let scatter_dir = random_vec_in_hemisphere(hit.normal.clone());
-        Some(ScatterRecord { 
-            ray: Ray::new(hit.point, scatter_dir), 
-            attenuation: self.albedo
+        let scatter_dir = hit.normal + random_vec_in_sphere();
+        let scatter_dir = if scatter_dir.magnitude2() < 0.000001 {
+            hit.normal
+        } else {
+            scatter_dir.normalize()
+        };
+        Some(ScatterRecord {
+            ray: Ray::new(hit.point, scatter_dir),
+            attenuation: self.albedo,
         })
     }
 }
