fde92b18a099d13779815c930db822bec81a924b
Aleksandr Lvov <alexlvov26@gmail.com>
2023-03-18 22:13:20 +0200

    remove Texture2

--- src/texture.rs
+++ src/texture.rs
@@ -2,29 +2,6 @@ use image::RgbImage;
 
 use crate::{rgb_to_vec, Colour};
 
-pub trait Texture2 {
-    fn colour_at(&self, u: f64, v: f64) -> Colour;
-}
-
-impl Texture2 for Colour {
-    fn colour_at(&self, _u: f64, _v: f64) -> Colour {
-        self.clone()
-    }
-}
-
-impl Texture2 for RgbImage {
-    fn colour_at(&self, u: f64, v: f64) -> Colour {
-        let (width, height) = self.dimensions();
-        let mut i = (u.clamp(0.0, 1.0) * (width as f64)) as u32;
-        let mut j = (v.clamp(0.0, 1.0) * (height as f64)) as u32;
-
-        i = if i >= width { i - 1 } else { i };
-        j = if j >= height { j - 1 } else { j };
-
-        rgb_to_vec(self.get_pixel(i, j))
-    }
-}
-
 #[derive(Clone)]
 pub enum Texture {
     Colour(Colour),
