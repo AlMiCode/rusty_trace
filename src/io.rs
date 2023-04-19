c7163306ed604096334ef6f186b6a97a0256f319
Aleksandr Lvov <alexlvov26@gmail.com>
2023-04-20 00:11:52 +0300

    small refactor on io.rs

--- src/io.rs
+++ src/io.rs
@@ -1,19 +1,7 @@
-use image::RgbImage;
+pub fn load_image(filename: String) -> Option<image::RgbImage> {
+    try_load_image(&filename.into()).ok()
+}
 
-pub fn load_image(filename: String) -> Option<RgbImage> {
-    let reader = match image::io::Reader::open(&filename) {
-        Err(_) => {
-            eprintln!("Could not read");
-            return None;
-        }
-        Ok(r) => r,
-    };
-    let dyn_img = match reader.decode() {
-        Err(_) => {
-            eprintln!("Could not decode");
-            return None;
-        }
-        Ok(img) => img,
-    };
-    Some(dyn_img.to_rgb8())
+pub fn try_load_image(filename: &std::path::PathBuf) -> image::ImageResult<image::RgbImage> {
+    Ok(image::io::Reader::open(filename)?.decode()?.to_rgb8())
 }
